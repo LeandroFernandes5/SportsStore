15302faf3ed2db6f53c2002c62a0b2b0a631dc61
Leandro Fernandes <fernandesleandro1996@gmail.com>
2017-11-02 10:34:06 +0000

    Aula2/11

---
+++ WebApplication1/Models/FakeProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Models
+{
+    public class FakeProductRepository : iProductRepository
+    {
+        public IEnumerable<Product> Products => new List<Product>
+        {
+            new Product {Name = "Footbal", Price = 25},
+            new Product {Name = "Surf Board", Price = 179},
+            new Product {Name = "Running Shoes", Price = 95},
+            new Product {Name = "Turf Tennis", Price = 65},
+            new Product {Name = "Gloves", Price = 17},
+        };
+    }
+}
---
+++ WebApplication1/Models/iProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Models
+{
+    public interface iProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
